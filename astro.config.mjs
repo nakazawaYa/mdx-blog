6aa6c5063bc98bb36ad8fcc4db7212e2076caa9a
nakazawaYa <nakazawa3719@gmail.com>
2024-06-07 00:42:03 +0900

    設定周り見直し

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   vite: {
     css: {
       preprocessorOptions: {
@@ -12,5 +14,5 @@ export default defineConfig({
         }
       }
     }
-  },
+  }
 });
