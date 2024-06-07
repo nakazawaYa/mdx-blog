069d74263af9c7c5d0fc11c12f3ecf0396735b3c
nakazawaYa <nakazawa3719@gmail.com>
2024-06-07 22:47:28 +0900

    gtag 追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     css: {
       preprocessorOptions: {
