8c6bf9ce72a31f67c27cbde0a9fb727f92187e7e
nakazawaYa <nakazawa3719@gmail.com>
2024-07-21 00:48:10 +0900

    一時

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
     config: {
       forward: ["dataLayer.push"]
     }
-  }), mdx(), sitemap()],
+  }), mdx(), sitemap(), react()],
   vite: {
     css: {
       preprocessorOptions: {
